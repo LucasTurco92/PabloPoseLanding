4e86c70352a9f2644d5306fa63245dcd2183beda
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2020-09-06 03:33:46 -0300

    change public for videos 1.1

--- components/third-section/third-section.jsx
+++ components/third-section/third-section.jsx
@@ -7,9 +7,9 @@ const ThirdSection = (props) => {
         <div className={style.thirdContainer}>
             <div className={style.third}>     
                 <dir className={style.videoSection}>
-                <VideoContainer url='/caso/caso.mp4'  title='test' paragraph='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis similique id error, soluta dolorem ad! Placeat totam magni officiis ducimus minus iste, eveniet, voluptatem tempore praesentium illum culpa corporis quam!'/>
-                <VideoContainer url='/esperanza/esperanza.mp4' title='test' paragraph='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis similique id error, soluta dolorem ad! Placeat totam magni officiis ducimus minus iste, eveniet, voluptatem tempore praesentium illum culpa corporis quam!'/>
-                <VideoContainer url='/Secuencia/Secuencia479.mp4' title='test' paragraph='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis similique id error, soluta dolorem ad! Placeat totam magni officiis ducimus minus iste, eveniet, voluptatem tempore praesentium illum culpa corporis quam!'/>
+                <VideoContainer url='/caso.mp4'  title='test' paragraph='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis similique id error, soluta dolorem ad! Placeat totam magni officiis ducimus minus iste, eveniet, voluptatem tempore praesentium illum culpa corporis quam!'/>
+                <VideoContainer url='/esperanza.mp4' title='test' paragraph='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis similique id error, soluta dolorem ad! Placeat totam magni officiis ducimus minus iste, eveniet, voluptatem tempore praesentium illum culpa corporis quam!'/>
+                <VideoContainer url='/Secuencia479.mp4' title='test' paragraph='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis similique id error, soluta dolorem ad! Placeat totam magni officiis ducimus minus iste, eveniet, voluptatem tempore praesentium illum culpa corporis quam!'/>
                 </dir>
             </div>
     
