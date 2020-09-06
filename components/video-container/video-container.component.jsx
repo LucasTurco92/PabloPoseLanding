7c96efea45d3637baaf5f7f3369fe8ecab336977
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2020-09-06 03:07:41 -0300

    videos fix 1.0

--- components/video-container/video-container.component.jsx
+++ components/video-container/video-container.component.jsx
@@ -7,7 +7,7 @@ const VideoContainer = (props) => {
     return (
         <div className={styles.container}>
             <div className={styles.videoContainer}>
-                <video width={videoWidth}  src={url} autoPlay="autoPlay" loop="loop" muted type="video/mp4">
+                <video width={videoWidth}  src={url} autostart="true" autoPlay="true" loop="loop" muted="true" type="video/mp4">
                     Tu navegador no implementa el elemento <code>video</code>
                 </video>
             </div>
