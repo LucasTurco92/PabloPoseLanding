bc01623eed17cf23ae05d440aeab2520d0b5f9f5
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2020-09-09 16:32:27 -0300

    footer component refactored

--- components/footer/footer.component.jsx
+++ components/footer/footer.component.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import FooterContact from '../footer-contact/footer-contact.component';
 import styles from './footer.module.scss';
 
 const Footer = () => (
     <div className={styles.footer}>
-        <FooterContact />
+         <div className={styles.container}>
+        <div className={styles.icons}>
+            <ul>
+                <li><a href="https://twitter.com/posepablo?lang=es " className={styles.twitter}><img src='/socialIcons/twitter-brands.svg' alt="twitter" /></a></li>
+                <li><a href="https://www.facebook.com/pablo.poseiglesias.1 " className={styles.facebook}> <img src='/socialIcons/facebook-f-brands.svg' alt="facebook" /></a></li>
+                <li><a href="https://www.instagram.com/pablopose_/?hl=es " className={styles.instagram}> <img src='/socialIcons/instagram-brands.svg' alt="instagram" /></a></li>
+            </ul>
+        </div>
+    </div>
     </div>
 )
 
