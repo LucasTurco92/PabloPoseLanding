dd5790b6077c4c1e87e4acf7286cb63b993bbf92
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2020-09-16 00:00:45 -0300

    Linkedin and youtube icon added

--- components/social/social.component.jsx
+++ components/social/social.component.jsx
@@ -10,6 +10,8 @@ const Social = () => (
                 <li><a href="https://twitter.com/posepablo?lang=es " className={styles.twitter}><img src='/socialIcons/twitter-brands.svg' alt="twitter" /></a></li>
                 <li><a href="https://www.facebook.com/pablo.poseiglesias.1 " className={styles.facebook}> <img src='/socialIcons/facebook-f-brands.svg' alt="facebook" /></a></li>
                 <li><a href="https://www.instagram.com/pablopose_/?hl=es " className={styles.instagram}> <img src='/socialIcons/instagram-brands.svg' alt="instagram" /></a></li>
+                <li><a href="https://www.facebook.com/pablo.poseiglesias.1 " className={styles.facebook}> <img src='/socialIcons/linkedin-brands.svg' alt="facebook" /></a></li>
+                <li><a href="https://www.instagram.com/pablopose_/?hl=es " className={styles.youtube}> <img src='/socialIcons/youtube-brands.svg' alt="instagram" /></a></li>
             </ul>
         </div>
     </div>
