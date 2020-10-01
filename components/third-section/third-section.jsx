6878be8306194d34f1c46071e9e8ce2b7f35d464
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2020-10-01 01:18:38 -0300

    videos styles updated

--- components/third-section/third-section.jsx
+++ components/third-section/third-section.jsx
@@ -8,9 +8,9 @@ const ThirdSection = (props) => {
             <div className={style.third}>     
             <span className={style.title}>Lorem</span> 
                 <dir className={style.videoSection}>
-                <VideoContainer url='/caso.mp4'  title='test' paragraph='Lorem isum, dolor sit amet consectetur adipisicing elit. Perspiciatis similique id error, soluta dolorem ad! Placeat totam magni officiis ducimus minus iste, eveniet, voluptatem tempore praesentium illum culpa corporis quam!'/>
-                <VideoContainer url='/esperanza.mp4' title='test' paragraph='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis similique id error, soluta dolorem ad! Placeat totam magni officiis ducimus minus iste, eveniet, voluptatem tempore praesentium illum culpa corporis quam!'/>
-                <VideoContainer url='/secuencia.mp4' title='test' paragraph='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis similique id error, soluta dolorem ad! Placeat totam magni officiis ducimus minus iste, eveniet, voluptatem tempore praesentium illum culpa corporis quam!'/>
+                <VideoContainer url='/caso.mp4' invert={false}  title='test' paragraph='Lorem isum, dolor sit amet consectetur adipisicing elit. Perspiciatis similique id error, soluta dolorem ad! Placeat totam magni officiis ducimus minus iste, eveniet, voluptatem tempore praesentium illum culpa corporis quam!'/>
+                <VideoContainer url='/esperanza.mp4' invert={true} title='test' paragraph='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis similique id error, soluta dolorem ad! Placeat totam magni officiis ducimus minus iste, eveniet, voluptatem tempore praesentium illum culpa corporis quam!'/>
+                <VideoContainer url='/secuencia.mp4' invert={false} title='test' paragraph='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis similique id error, soluta dolorem ad! Placeat totam magni officiis ducimus minus iste, eveniet, voluptatem tempore praesentium illum culpa corporis quam!'/>
                 </dir>
             </div>
         </div>
--- components/video-container/video-container.component.jsx
+++ components/video-container/video-container.component.jsx
@@ -2,29 +2,31 @@ import React from 'react';
 import styles from './video-container.module.scss';
 
 const VideoContainer = (props) => {
-    const { url, title, paragraph, videoWidth } = props;
-
+    const { url, title, paragraph, videoWidth, invert } = props;
+    const container = invert ? `${styles.container} ${styles.invert}` : `${styles.container} ${styles.normal}`;
+    console.log(container, invert);
+    
     return (
-        <div className={styles.container}>
+        <div className={container}>
             <div className={styles.borderContainer}>
-
                 <div className={styles.videoContainer}>
-                
-                        <video width={videoWidth} src={url} playsinline="true" autostart="true" autoPlay="true" loop="true" muted="true" type="video/mp4">
-                            Tu navegador no implementa el elemento <code>video</code>
-                        </video>
-
-                    
+                    <video width={videoWidth} src={url} playsinline="true" autostart="true" autoPlay="true" loop="true" muted="true" type="video/mp4">
+                        Tu navegador no implementa el elemento <code>video</code>
+                    </video>
                 </div>
-
-
-                <div className={styles.text}>
+                <div className={styles.textMobile}>
                     <h1> {title}</h1>
                     <p>
                         {paragraph}
                     </p>
                 </div>
             </div>
+                <div className={styles.text}>
+                    <h1> {title}</h1>
+                    <p>
+                        {paragraph}
+                    </p>
+                </div>
         </div>
     )
 }
