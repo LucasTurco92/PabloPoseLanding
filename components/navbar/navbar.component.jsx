4d1c833e29a2f44fb83414f3869717776f70f4d6
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2020-10-04 20:33:48 -0300

    videos, footer and navbar updated

--- components/navbar/navbar.component.jsx
+++ components/navbar/navbar.component.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import Social from '../social/social.component';
+import SocialIcons from '../social-icons/social-icons.component';
+
 import styles from './navbar.module.scss';
 
 
 const Navbar = () => {
     return (
         <div className={styles.navbarContainer}>
-            <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Poiret+One&display=swap" rel="stylesheet"/>
+            <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Poiret+One&display=swap" rel="stylesheet" />
 
-            <h1 ><span className={styles.name}>Pablo Pose</span><span className={styles.profesion}> </span></h1>
-            <Social/>
+            <h1 ><span className={styles.name}>Pablo Pose</span></h1>
+            <div className={styles.socialContainer}>
+                <SocialIcons />
+            </div>
         </div>
     )
 }
