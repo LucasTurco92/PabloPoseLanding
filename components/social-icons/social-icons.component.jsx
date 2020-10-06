ee01f2ba980304738968590f18d9a32ff802981e
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2020-10-05 22:29:49 -0300

    linkedin icon updated

--- components/social-icons/social-icons.component.jsx
+++ components/social-icons/social-icons.component.jsx
@@ -7,7 +7,7 @@ const SocialIcons = () => (
                     <li><a href="https://twitter.com/posepablo?lang=es " className={styles.twitter}><img src='/socialIcons/twitter-brands.svg' alt="twitter" /></a></li>
                     <li><a href="https://www.facebook.com/pablo.poseiglesias.1 " className={styles.facebook}> <img src='/socialIcons/facebook-f-brands.svg' alt="facebook" /></a></li>
                     <li><a href="https://www.instagram.com/pablopose_/?hl=es " className={styles.instagram}> <img src='/socialIcons/instagram-brands.svg' alt="instagram" /></a></li>
-                    <li><a href="https://www.facebook.com/pablo.poseiglesias.1 " className={styles.facebook}> <img src='/socialIcons/linkedin-brands.svg' alt="facebook" /></a></li>
+                    <li><a href="https://www.facebook.com/pablo.poseiglesias.1 " className={styles.linkedIn}> <img src='/socialIcons/linkedin-brands.svg' alt="facebook" /></a></li>
                     <li><a href="https://www.instagram.com/pablopose_/?hl=es " className={styles.youtube}> <img src='/socialIcons/youtube-brands.svg' alt="instagram" /></a></li>
                 </ul>
             </div>
