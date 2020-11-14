73235c2cc1671959a60cf81848ba8d4bb4f447b0
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2020-11-14 20:52:06 -0300

    footer mobile first refactored

--- components/footer/footer.component.jsx
+++ components/footer/footer.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Icon from "../icon/icon.component";
 import styles from './footer.module.scss';
 
 const Footer = () =>{
     return(
         <div className={styles.footer}>
             <div  className={styles.op}>
-            <a href="https://tempweb.com.ar"> <img src="/logo/logo.png"></img></a>
+                <Icon alt='tempweb' source='/logo/logo.png' url=''/>
             </div>
         </div>
         )
