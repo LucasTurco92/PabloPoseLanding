7701a2c20866d9fefb7d6054fd1ddefac62eefcc
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2020-11-15 16:30:43 -0300

    Full Mobile First

--- components/third-section/third-section.jsx
+++ components/third-section/third-section.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import style from './third-section.module.scss';
 import VideoContainer from '../video-container/video-container.component';
 
-const ThirdSection = (props) => {
+const ThirdSection = () => {
     return (
         <div className={style.thirdContainer}>
             <div className={style.third}>     
-            <span className={style.title}>Destacados</span> 
                 <dir className={style.videoSection}>
-                <VideoContainer url='/esperanza.mp4'  title='test' paragraph='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis similique id error, soluta dolorem ad! Placeat totam magni officiis ducimus minus iste, eveniet, voluptatem tempore praesentium illum culpa corporis quam!'/>
-                <VideoContainer url='/caso.mp4'  title='test' paragraph='Lorem isum, dolor sit amet consectetur adipisicing elit. Perspiciatis similique id error, soluta dolorem ad! Placeat totam magni officiis ducimus minus iste, eveniet, voluptatem tempore praesentium illum culpa corporis quam!'/>
-                <VideoContainer url='/secuencia.mp4' title='test' paragraph='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis similique id error, soluta dolorem ad! Placeat totam magni officiis ducimus minus iste, eveniet, voluptatem tempore praesentium illum culpa corporis quam!'/>
+                <VideoContainer url='/esperanza.mp4'  title='Esperanza' />
+                <VideoContainer url='/caso.mp4'  title='O caso Diana Quer'/>
+                <VideoContainer url='/secuencia.mp4' title='479' />
                 </dir>
             </div>
         </div>
