af023ba6582e338c419a7bb37ec1e9a8f94af461
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2020-11-15 04:01:54 -0300

    fix mobile fixed elements and title icon deleted

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Intro from "../components/intro/intro.component";
 import Social from "../components/social/social.component";
-import TitleIcon from "../components/title-icon/title-icon";
 import Footer from "../components/footer/footer.component";
 import SecondSection from "../components/second-section/second-section.component";
 import Navbar from "../components/navbar/navbar.component";
@@ -37,7 +36,9 @@ export default () => (
       href="https://fonts.googleapis.com/css2?family=Sorts+Mill+Goudy:ital@1&display=swap"
       rel="stylesheet"
     ></link>
-    <TitleIcon />
+    <Head>
+      <link rel="icon" href="/title/theater-masks.svg" type="image/x-icon" />
+    </Head>
     <Head>
       <title>Pablo Pose</title>
     </Head>
