36a6d17bc4b12ef94643277f5d4cce86e542b9c4
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2020-11-15 03:52:46 -0300

    details fixed of separator

--- components/separator/separator.component.jsx
+++ components/separator/separator.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Styles from './separator.module.scss';
 
-const Separator = ({style}) =>(<div className={`${Styles.imgIntro} ${style}` } />)
+const Separator = ({style}) =>(<div className={`${Styles.separator} ${style}` } />)
 
 export default Separator;
