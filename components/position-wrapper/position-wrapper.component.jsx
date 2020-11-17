03c905fadd4eceebabee22e8d9c878cf73e257ff
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2020-11-17 00:29:55 -0300

    log deleted and hooks unnecessary removed

--- components/position-wrapper/position-wrapper.component.jsx
+++ components/position-wrapper/position-wrapper.component.jsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, {useRef } from "react";
 import { useScrollPosition } from "../commons/scroll-position.jsx";
 
 
 const PositionWrapper = (props) => {
   const ref = useRef();
-  const [position, setPosition] = useState(0);
   const action =  {
     actionUp:props.actionUp
     , actionDown:props.actionDown
@@ -12,12 +11,12 @@ const PositionWrapper = (props) => {
 
   useScrollPosition( 
     ({currPos,prevPos}) => {
-      let {top, height} = position;
+      let {top, height} = {top: ref.current.offsetTop, height:ref.current.offsetHeight};
       let newPosition = top+height;
       let posChange = currPos.y>=(newPosition)/2-100 && currPos.y<=height;
      (prevPos.y < currPos.y) && posChange ?  window.scrollTo({top: newPosition,left:0,behavior: 'smooth'}): '';
      (prevPos.y > currPos.y)  && posChange ? ref.current.scrollIntoView({ behavior: 'smooth', block:'start', top: 'start',}) : '';
-      console.table('top: ',top,'height: ', height,'curr: ',currPos.y,'prev: ',prevPos.y,'POS: ',(newPosition)/2-50);
+      //console.table('top: ',top,'height: ', height,'curr: ',currPos.y,'prev: ',prevPos.y,'POS: ',(newPosition)/2-50);
     },
     [''],
     null,
@@ -25,9 +24,6 @@ const PositionWrapper = (props) => {
    400
 )
 
-  useEffect(() => {
-    setPosition({top: ref.current.offsetTop, height:ref.current.offsetHeight}); 
-  }, []);
 
   return (<div ref={ref}>{props.children}</div>);
 };
